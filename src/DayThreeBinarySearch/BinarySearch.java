8607780952911bde1c25dfb97ed1dbf54638f36e
Suzanne Richman <suzanne@favoredfortune.com>
2018-07-11 22:53:05 -0700

    wrote challenge out but INTELLJ hates one of my pass-by-value variables that is used by a helper method and I can't figure out why. Will revisit in the a.m.

--- src/DayThreeBinarySearch/BinarySearch.java
+++ src/DayThreeBinarySearch/BinarySearch.java
@@ -2,11 +2,53 @@ package DayThreeBinarySearch;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] sortedArray;
-        String[] searchKey;
+         int[] sortedArray = {4,8,15,16,23,42};
+         int key =15;
+         searchArray(sortedArray, key);
     }
+    public static int searchArray(int[] sortedArray, int key){
+        int m = sortedArray.length/2;
+        for (int i =0; i<m; i++){
+            if( sortedArray[i] == key){
+                return i;
+            } else if ( sortedArray[i] < key){
+                int k = i/2 +1;
+                for(int j = 0; j<k; j++) {
+                    if (sortedArray[j] == key) {
+                        return j;
+                    } else {
+                        return -1;
+                    }
+                    if (sortedArray[i] > key) {
+                        int r = i * 2;
+                        for (int q = 0; q < r; q++) {
+                            if (sortedArray[q] == key) {
+                                return q;
+                            } else {
+                                return -1;
+                            }
+                        }
+                    }
+                }
+            } searchHelperRight(m);
+        }
+    }
+    public static int searchHelperRight(int r, int[] sortedArray, int key) {
+        int e = (sortedArray.length - 1) - r;
+        for (int i = r; i > e; i++) {
+            if (sortedArray[i] == key) {
+                return i;
+            } else if (sortedArray[i] > key) {
+                int r = i * 2;
+                for (int q = 0; q < r; q++) {
+                    if (sortedArray[q] == key) {
+                        return q;
+                    } else {
+                        return -1;
+                    }
+                }
+            }
 
-    public int searchArray(int[] sortedArray){
-
+        } return 0;
     }
 }
