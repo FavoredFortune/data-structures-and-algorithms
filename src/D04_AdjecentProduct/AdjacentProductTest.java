3d87d647b7e6e8782ddd74ff9e116a202155c693
Suzanne Richman <suzanne@favoredfortune.com>
2018-07-13 11:21:22 -0700

    got all tests passing!

--- src/D04_AdjecentProduct/AdjacentProductTest.java
+++ src/D04_AdjecentProduct/AdjacentProductTest.java
@@ -7,30 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class AdjacentProductTest {
 
     @Test
-    void largestProduct() {
-        //test values
-        int[][] superArray = {
-                {67,68,29,53},
-                {2,4,16,7,8},
-                {43,65,77,88}
-        };
-
-        //expected and actual values for test method
-        int expected = 4556;
-        int actual = AdjacentProduct.LargestProduct(superArray);
-
-        //test method comparing two integers
-        assertEquals(expected, actual);
-    }
-
-    @Test
     void largestProduct1() {
         //test values
         int[][] superArray = {
-                {1,2,1},
-                {2,4,2},
-                {3,6,8},
-                {7,8,1}
+                {1, 2, 1},
+                {2, 4, 2},
+                {3, 6, 8},
+                {7, 8, 1}
         };
 
         //expected and actual values for test method
@@ -45,14 +28,13 @@ class AdjacentProductTest {
     void largestProduct2() {
         //test values
         int[][] superArray = {
-                {12, 20, 13, 44},
-                {2,4,2,7},
-                {3,6,8, 10},
-                {7,8,1, 1}
+                {2, 4, 2, 7},
+                {3, 6, 8, 10},
+                {7, 8, 1, 1}
         };
 
         //expected and actual values for test method
-        int expected = 572;
+        int expected = 80;
         int actual = AdjacentProduct.LargestProduct(superArray);
 
         //test method comparing two integers
@@ -60,34 +42,20 @@ class AdjacentProductTest {
     }
 
     @Test
-    void greatestValue() {
+    void largestProduct3() {
+        //test values
         int[][] superArray = {
-                {67,68,29,53},
-                {2,4,16,8},
-                {43,65,77,88}
+                {2, 4, 2, 7},
+                {7, 8, 1, 1},
+                {3, 2, 8, 20},
+                {7, 8, 1, 1}
         };
 
-        int expected = 4556;
-        int actual = AdjacentProduct.greatestValue(superArray,0,0 );
+        //expected and actual values for test method
+        int expected = 160;
+        int actual = AdjacentProduct.LargestProduct(superArray);
 
         //test method comparing two integers
         assertEquals(expected, actual);
-
-    }
-
-    @Test
-    void greatestValue1() {
-        int[][] superArray = {
-                {1,1,1,53},
-                {2,1,1,1},
-                {1,2,3,8}
-        };
-
-        int expected = 53;
-        int actual = AdjacentProduct.greatestValue(superArray,0,0 );
-
-        //test method comparing two integers
-        assertEquals(expected, actual);
-
     }
 }
