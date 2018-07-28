358321bf32ab6a449ce0d63d64ed5ccb3e26e684
Suzanne Richman <suzanne@favoredfortune.com>
2018-07-27 21:47:21 -0700

    getting started on rebuilding this whiteboard. my written whiteboard is so bad and I wish I'd saved my finished whiteboard somewhere else before everything went kablewy. will get fresh start in the morning.

--- src/D08_MergeLinkedLists/MergedLinkedLists.java
+++ src/D08_MergeLinkedLists/MergedLinkedLists.java
@@ -1,10 +1,27 @@
 package D08_MergeLinkedLists;
 import D06_Insertions.*;
+
+import java.lang.reflect.Array;
+
 public class MergedLinkedLists {
 
+    public static ListNode mergeLists(LinkedList listA, LinkedList listB) {
+    int aSize = listA.size();
+    int bSize = listB.size();
+    String response = "";
 
-//
-//public static class LinkedList mergeLists(LinkedList A, LinkedList B) {
-//
+    if(aSize == 0 && bSize == 0){
+        System.out.println("Lists are empty");
+    }
+    if(aSize == 0 || bSize == 0){
+        while(aSize>0 || bSize>0){
+            if(aSize>0){
+                String[] listForRoot = (String[]) listA;
+                return listForRoot[0];
+            }
+        }
+    }
+    }
+
 
 }
