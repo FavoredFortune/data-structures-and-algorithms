4518a91c93820e951c780d250509b0c7d3e1090e
Suzanne Richman <suzanne@favoredfortune.com>
2018-07-29 21:48:47 -0700

    after trying it the way I know was wrong, from my whiteboard, but wanting to understand why it was wrong - per class review - I followed lecture review of Panos (who initially used method similar to mine with 4 stacks) and Brandon Fenty approaches. I then rewrote my approach based on Brandons, making comments along the way in my own language to make sure I understood what was happening. Two of my tests still initially broke, but I found my error in logic - and missing brace - and finally, ALL tests pass!

--- src/D13_MultiBracketValidation/MultiBracketValidation.java
+++ src/D13_MultiBracketValidation/MultiBracketValidation.java
@@ -4,66 +4,59 @@ import java.util.Stack;
 
 public class MultiBracketValidation {
 
-    //instantiate holder stacks for sorting
-    public static Stack<String> curly = new Stack<>();
-    public static  Stack<String> bracket = new Stack<>();
-    public static  Stack<String> parens = new Stack<>();
-    public static Stack<String> other = new Stack<>();
+    public static boolean multiBracketValidation(String input){
 
-    public static boolean multiBracketValidation(Stack<String> input){
-        Stack<String> original = input;
+        //per class review, nixed additional stack method approach
+        //using single stack in single method
+        //like cohort member, Brandon Fenty
+        Stack<Character> inputString = new Stack<>();
 
-        //apply helper method with helper method to execute main intended method
-        return bracketSort(original);
-    }
+        boolean result = true;
 
-    public static boolean bracketSort(Stack<String> input) {
-        int size = input.size();
-
-        //rule out empty case
-        if (size == 0) {
-            return true;
+        //adding back in for edge case of empty string
+        // but with different method based on lecture review
+        if(input.isEmpty()){
+            return result;
         }
 
-        //while the original string stack isn't empty do the following
-        while (size > 0) {
+        /*
+        while the original string isn't empty
+        go through the string this way
+        */
+        for(int i = 0; i < input.length(); i++){
 
-            //evalute the first item in the input string stack
-            String temp = input.pop();
+            //instantiate a variable for the first item in the input string
+            //and to track each character in the string as you move through it
+            char temp = input.charAt(i);
 
-            //sort into various stacks by character type
-            if (temp == "{" || temp == "}") {
-                curly.push(temp);
-            } else if (temp == "[" || temp == "]") {
-                bracket.push(temp);
-            } else if (temp == "(" || temp == ")") {
-                parens.push(temp);
-            } else {
-                other.push(temp);
+            //check for beginning type brackets push back into stack if existing.
+            if (temp == '{' || temp == '[' || temp == '(') {
+                inputString.push(temp);
+            }
+            //check for closing type brackets and push back into stack if existing.
+            if (temp == '}' || temp == ']' || temp == ')') {
+                if (inputString.isEmpty()) {
+                    return false;
+                }
+
+                //track the new element in the input string stack
+                char newChar = inputString.peek();
+
+                //next, compare the first character in the stack with the current character
+                //to see if they match or not
+                //if the two variables do match pop the character back out of the stack
+                if (newChar == '{' && temp == '}' || newChar == '[' && temp == ']' || newChar == '(' && temp == ')') {
+                    inputString.pop();
+                }
+                //but if match isn't found input string isn't balanced in order so return result
+                else {
+                    return false;
+                }
+
             }
         }
-
-        //evaluate with secondary helper method if each character bracket type is matched/even
-        if (curly.matched() == true && bracket.matched() == true && parens.matched() == true) {
-            return true;
-        } else {
-            return false;
-        }
+        return result;
     }
-
-    //see with helper method if each character bracket type is matched/even
-    public static boolean matched(){
-        boolean match = false;
-        while (match) {
-            if (curly.size() % 2 == 0 && bracket.size() % 2 == 0 && parens.size() % 2 == 0) {
-                match = true;
-            } else {
-                match = false;
-            }
-        }
-        return match;
-    }
-
 }
 
 
