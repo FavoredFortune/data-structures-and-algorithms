073c2d7a089c53e6f48c95cee1845eede93146c8
Suzanne Richman <suzanne@favoredfortune.com>
2018-08-26 12:51:54 -0700

    updating with new content

---
+++ src/D34_Interviews/FindUnique.java
@@ -0,0 +1,22 @@
+package D34_Interviews;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//just scratching out Amy's solution for my own reference. I thought it worked and it doesn't.
+
+public class FindUnique {
+
+    public static boolean isUnique(String string){
+
+        boolean isUnique = true;
+
+        String[] uniqueChars = string.split("");
+
+        Set<Character> characters = new HashSet<>();
+
+
+
+
+    }
+}
