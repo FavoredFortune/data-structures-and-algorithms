2d0f4ed095e0490e719a15c8e00928051586f58e
Suzanne Richman <suzanne@favoredfortune.com>
2018-09-03 19:25:40 -0700

    created test for this whiteboard and found my original logic sound! YAY

---
+++ src/D11_Queue_with_Stacks/ListNode.java
@@ -0,0 +1,11 @@
+public class ListNode {
+    public int data;
+    public ListNode next;
+    public ListNode root;
+
+    public ListNode(int data) {
+        this.data = data;
+        this.next = null;
+        this.root = null;
+    }
+}
